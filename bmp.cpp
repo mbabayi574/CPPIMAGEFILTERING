97970d330e5c8b2971f8891c4a091e7c9a92aad6
Bobby <m.babayi574@gmail.com>
2023-12-14 16:31:21 +0330

    Update user authentication logic

--- bmp.cpp
+++ bmp.cpp
@@ -3,101 +3,123 @@
 #include <algorithm>
 #include <fstream>
 
-Pixel::Pixel(uint8_t r, uint8_t g, uint8_t b)
+Pixel::Pixel(uint8_t r, uint8_t g, uint8_t b) 
     : blu(b), grn(g), red(r) {}
 
-bool create(Bmp& bmp, int width, int height) {
-    bmp.validState = false;
-    bmp.padding = calcBmpPadding(width);
+bool Bmp::create(int width, int height) {
+  validState = false;
+  padding = calcBmpPadding(width);
 
-    bmp.hdr.fileType = BMP_FILE_TYPE;
-    bmp.hdr.fileSize = sizeof(Header) + sizeof(InfoHeader) + (width + bmp.padding) * height * sizeof(Pixel);
-    bmp.hdr.reserved1 = 0;
-    bmp.hdr.reserved2 = 0;
-    bmp.hdr.offset = sizeof(Header) + sizeof(InfoHeader);
+  hdr.fileType = BMP_FILE_TYPE;
+  hdr.fileSize = sizeof(Header) + sizeof(InfoHeader) + (width + padding) * height * sizeof(Pixel);
+  hdr.reserved1 = 0;
+  hdr.reserved2 = 0;
+  hdr.offset = sizeof(Header) + sizeof(InfoHeader);
 
-    delete[] bmp.fileData;
-    bmp.fileData = new (std::nothrow) char[bmp.hdr.fileSize];
-    if (bmp.fileData == nullptr) return false;
-    std::fill(bmp.fileData, bmp.fileData + bmp.hdr.fileSize, 0x00);
+  delete[] fileData;
+  fileData = new (std::nothrow) char[hdr.fileSize];
+  if (fileData == nullptr) return false;
+  std::fill(fileData, fileData + hdr.fileSize, 0x00);
 
-    bmp.infoHdr = {0};
-    bmp.infoHdr.headerSize = sizeof(InfoHeader);
-    bmp.infoHdr.width = width;
-    bmp.infoHdr.height = height;
-    bmp.infoHdr.planes = 1;
-    bmp.infoHdr.bitCount = 24;
-    bmp.infoHdr.imageSize = (width + bmp.padding) * height * sizeof(Pixel);
+  infoHdr.headerSize = sizeof(InfoHeader);
+  infoHdr.width = width;
+  infoHdr.height = height;
+  infoHdr.planes = 1;
+  infoHdr.bitCount = 24;
+  infoHdr.compression = 0; 
+  infoHdr.imageSize = (width + padding) * height * sizeof(Pixel);
 
-    std::copy((char*)&bmp.hdr, (char*)&bmp.hdr + sizeof(Header), bmp.fileData);
-    std::copy((char*)&bmp.infoHdr, (char*)&bmp.infoHdr + sizeof(InfoHeader), bmp.fileData + sizeof(Header));
+  std::copy((char*)&hdr, (char*)&hdr + sizeof(Header), fileData);
+  std::copy((char*)&infoHdr, (char*)&infoHdr + sizeof(InfoHeader), fileData + sizeof(Header));
 
-    bmp.validState = true;
-    bmp.data.assign(height, std::vector<Pixel>(width));
-    return true;
+  validState = true;
+  data.assign(height, std::vector<Pixel>(width));
+  return true;
 }
 
-ReadResult read(Bmp& bmp, const std::string& filename) {
-    std::ifstream file(filename, std::ios_base::binary);
-    if (!file.is_open()) return ReadResult::open_err;
-    bmp.validState = false;
+bool Bmp::read(const std::string& filename) {
+  std::ifstream file(filename, std::ios_base::binary);
+  if(!file.is_open()) return false; 
+  
+  file.read((char*)&hdr, sizeof(Header));
+  file.read((char*)&infoHdr, sizeof(InfoHeader));
+  
+  if (hdr.fileType != BMP_FILE_TYPE) return false;
+  if (infoHdr.bitCount != 24 || infoHdr.compression != 0) return false;
+  padding = calcBmpPadding(infoHdr.width);
 
-    file.read((char*)&bmp.hdr, sizeof(Header));
-    file.read((char*)&bmp.infoHdr, sizeof(InfoHeader));
+  file.seekg(0, std::ios_base::end);
+  std::streampos length = file.tellg();
+  file.seekg(0, std::ios_base::beg);
 
-    if (bmp.hdr.fileType != BMP_FILE_TYPE) return ReadResult::invalid_file;
-    if (bmp.infoHdr.bitCount != 24 || bmp.infoHdr.compression != 0) return ReadResult::unsupported_bmp;
-    bmp.padding = calcBmpPadding(bmp.infoHdr.width);
+  delete[] fileData;
+  fileData = new (std::nothrow) char[length];
+  if (fileData == nullptr) return false;
+  file.read(fileData, length);
+  
+  validState = true;
+  fileDataToVector();
 
-    file.seekg(0, std::ios_base::end);
-    std::streampos length = file.tellg();
-    file.seekg(0, std::ios_base::beg);
-
-    delete[] bmp.fileData;
-    bmp.fileData = new (std::nothrow) char[length];
-    if (bmp.fileData == nullptr) return ReadResult::alloc_err;
-    file.read(bmp.fileData, length);
-
-    bmp.validState = true;
-    fileDataToVector(bmp);
-    return ReadResult::success;
+  file.close();
+  
+  return true;
 }
 
-bool write(Bmp& bmp, const std::string& filename) {
-    if (!bmp.validState) return false;
-    vectorToFileData(bmp);
+bool Bmp::write(const std::string& filename) {
 
-    std::ofstream file(filename, std::ios_base::binary);
-    if (!file.is_open()) return false;
+  std::ofstream file(filename, std::ios_base::binary);
+  if(!file.is_open()) return false;
+  
+  std::copy((char*)&hdr, (char*)&hdr + sizeof(Header), fileData);
+  std::copy((char*)&infoHdr, (char*)&infoHdr + sizeof(InfoHeader), fileData + sizeof(Header));
+  
+  vectorToFileData();
 
-    file.write(bmp.fileData, bmp.hdr.fileSize);
-    return true;
+  file.write(fileData, hdr.fileSize);
+
+  file.close();
+
+  return true;
+}
+
+int Bmp::getWidth() const {
+  return infoHdr.width; 
+}
+
+int Bmp::getHeight() const {
+  return infoHdr.height;
+}
+   
+Pixel& Bmp::getPixel(int row, int col) {
+  const int rowStart = (infoHdr.height - 1 - row) * (infoHdr.width * sizeof(Pixel) + padding);
+  char* pixelPos = fileData + hdr.offset + rowStart + col * sizeof(Pixel);
+  return *(Pixel*)pixelPos; 
+}
+
+void Bmp::setPixel(int row, int col, Pixel pixel) {
+  const int rowStart = (infoHdr.height - 1 - row) * (infoHdr.width * sizeof(Pixel) + padding);
+  char* pixelPos = fileData + hdr.offset + rowStart + col * sizeof(Pixel); 
+  *(Pixel*)pixelPos = pixel;
 }
 
 int calcBmpPadding(int width) {
-    const int bytesInRow = width * sizeof(Pixel);
-    return bytesInRow % 4 ? 4 - bytesInRow % 4 : 0;
+  const int bytesInRow = width * sizeof(Pixel);
+  return bytesInRow % 4 ? 4 - bytesInRow % 4 : 0;  
 }
 
-Pixel& getFilePixel(const Bmp& bmp, int row, int col) {
-    const int rowStart = (bmp.infoHdr.height - 1 - row) * (bmp.infoHdr.width * sizeof(Pixel) + bmp.padding);
-    char* const pixelPos = bmp.fileData + bmp.hdr.offset + rowStart + col * sizeof(Pixel);
-    return *(Pixel*)pixelPos;
-}
-
-void fileDataToVector(Bmp& bmp) {
-    bmp.data.assign(bmp.infoHdr.height, std::vector<Pixel>(bmp.infoHdr.width));
-    for (int row = 0; row < bmp.infoHdr.height; ++row) {
-        for (int col = 0; col < bmp.infoHdr.width; ++col) {
-            bmp.data[row][col] = getFilePixel(bmp, row, col);
-        }
+void fileDataToVector() {
+  data.assign(infoHdr.height, std::vector<Pixel>(infoHdr.width));
+  for (int row = 0; row < infoHdr.height; ++row) {
+      for (int col = 0; col < infoHdr.width; ++col) {
+          data[row][col] = getFilePixel(*this, row, col); 
     }
+  }
 }
 
-void vectorToFileData(Bmp& bmp) {
-    for (int row = 0; row < bmp.infoHdr.height; ++row) {
-        for (int col = 0; col < bmp.infoHdr.width; ++col) {
-            getFilePixel(bmp, row, col) = bmp.data[row][col];
-        }
+void vectorToFileData() {
+  for (int row = 0; row < infoHdr.height; ++row) {
+    for (int col = 0; col < infoHdr.width; ++col) {
+        getFilePixel(*this, row, col) = data[row][col];
     }
+  }
 }
